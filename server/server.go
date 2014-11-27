30aacef00fb0b151f8c3b4c44b47ed668e116fbc
Hugo Landau <hlandau@devever.net>
2014-11-27 20:36:01 +0000

    Fixes

--- server/server.go
+++ server/server.go
@@ -1,7 +1,7 @@
 package server
 
 import "github.com/hlandau/ncdns/namecoin"
-import "github.com/hlandau/ncdns/backend"
+import "github.com/hlandau/ncdns/ncdomain"
 import "github.com/hlandau/degoutils/log"
 import "github.com/miekg/dns"
 import _ "github.com/lib/pq"
@@ -10,6 +10,7 @@ import "regexp"
 import "fmt"
 import "strings"
 import "time"
+import "reflect"
 
 const mainNetGenesisHash = "000000000062b72c5e2ceb45fbc8587e807c155b0da735e6483dfba2f0a9c770"
 const genesisHeight = 0
@@ -42,7 +43,90 @@ type Config struct {
 	DatabaseURL         string `default:"" usage:"Database URL or other connection string (postgres: dbname=... user=... password=...)"`
 	Notify              bool   `default:"false" usage:"Whether to notify (for supported databases only)"`
 	NotifyName          string `default:"namecoin_updated" usage:"Event name to use for notification"`
+	NSECMode            string `default:"auto" usage:"NSEC mode to use (nsec3narrow, nsec3 or nsec)"`
 	StatusUpdateFunc    func(status string)
+}
+
+// Prepare statements
+type prepareds struct {
+	GetDomainID         *sql.Stmt `SELECT id FROM namecoin_domains WHERE name=$1 LIMIT 1`
+	InsertDomain        *sql.Stmt `INSERT INTO namecoin_domains (name, last_height, value) VALUES ($1,$2,$3) RETURNING id`
+	UpdateDomain        *sql.Stmt `UPDATE namecoin_domains SET last_height=$1, value=$2 WHERE id=$3`
+	DeleteDomainRecords *sql.Stmt `DELETE FROM records WHERE namecoin_domain_id=$1`
+	UpdateState         *sql.Stmt `UPDATE namecoin_state SET cur_block_hash=$1, cur_block_height=$2`
+	InsertPrevBlock     *sql.Stmt `INSERT INTO namecoin_prevblock (block_hash, block_height) VALUES ($1,$2) RETURNING id`
+	TruncatePrevBlock   *sql.Stmt `DELETE FROM namecoin_prevblock WHERE id < $1`
+	InsertRecord        *sql.Stmt `INSERT INTO records (domain_id,ttl,prio,namecoin_domain_id,name,type,content,auth,ordername) VALUES (1,600,$1,$2,$3,$4,$5,$6,$7)`
+	Rollback            *sql.Stmt `SELECT block_hash, block_height FROM namecoin_prevblock ORDER BY id DESC LIMIT 1`
+	RollbackPop         *sql.Stmt `DELETE FROM namecoin_prevblock WHERE id IN (SELECT id FROM namecoin_prevblock ORDER BY id DESC LIMIT 1)`
+	RollbackNewer       *sql.Stmt `SELECT id, name FROM namecoin_domains WHERE last_height > $1`
+	DeleteName          *sql.Stmt `DELETE FROM namecoin_domains WHERE id=$1`
+	Notify              *sql.Stmt `SELECT pg_notify('namecoin_updated', $1)`
+}
+
+func (p *prepareds) Prepare(db *sql.DB) error {
+	t := reflect.TypeOf(p).Elem()
+	v := reflect.Indirect(reflect.ValueOf(p))
+	nf := t.NumField()
+	for i := 0; i < nf; i++ {
+		f := t.Field(i)
+		if f.Tag == "" {
+			continue
+		}
+		pr, err := db.Prepare(string(f.Tag))
+		if err != nil {
+			fmt.Printf("error while preparing field %d", i+1)
+			return err
+		}
+		fv := v.Field(i)
+		fv.Set(reflect.ValueOf(pr))
+	}
+
+	return nil
+}
+
+func (p *prepareds) Close() error {
+	t := reflect.TypeOf(p).Elem()
+	v := reflect.Indirect(reflect.ValueOf(p))
+	nf := t.NumField()
+	for i := 0; i < nf; i++ {
+		f := t.Field(i)
+		if f.Tag == "" {
+			continue
+		}
+		fv := v.Field(i)
+		fvi := fv.Interface()
+		if fvi != nil {
+			if stmt, ok := fvi.(*sql.Stmt); ok {
+				stmt.Close()
+				fv.Set(reflect.ValueOf((*sql.Stmt)(nil)))
+			}
+		}
+	}
+	return nil
+}
+
+func (p *prepareds) Tx(tx *sql.Tx) (px *prepareds, err error) {
+	px = &prepareds{}
+	t := reflect.TypeOf(p).Elem()
+	v := reflect.Indirect(reflect.ValueOf(p))
+	v2 := reflect.Indirect(reflect.ValueOf(px))
+	nf := t.NumField()
+	for i := 0; i < nf; i++ {
+		f := t.Field(i)
+		if f.Tag == "" {
+			continue
+		}
+		fv := v.Field(i)
+		fv2 := v2.Field(i)
+		fvi := fv.Interface()
+		if fvi != nil {
+			if stmt, ok := fvi.(*sql.Stmt); ok {
+				fv2.Set(reflect.ValueOf(tx.Stmt(stmt)))
+			}
+		}
+	}
+	return
 }
 
 func Run(cfg Config, startedNotifyFunc func() error) error {
@@ -70,6 +154,32 @@ func Run(cfg Config, startedNotifyFunc func() error) error {
 		cfg.Notify = false
 	}
 
+	if cfg.NSECMode == "" || cfg.NSECMode == "auto" {
+		ns3p := ""
+		err := db.QueryRow("SELECT content FROM domainmetadata WHERE kind='NSEC3PARAM' and domain_id=1 LIMIT 1").Scan(&ns3p)
+		if err != nil {
+			cfg.NSECMode = "nsec3narrow"
+		} else {
+			cfg.NSECMode = "nsec3"
+		}
+	}
+
+	switch cfg.NSECMode {
+	case "nsec3narrow":
+	case "nsec3":
+		ns3p := ""
+		err := db.QueryRow("SELECT content FROM domainmetadata WHERE kind='NSEC3PARAM' AND domain_id=1 LIMIT 1").Scan(&ns3p)
+		if err == sql.ErrNoRows {
+			return fmt.Errorf("PowerDNS must have NSEC3PARAM option for domain set to '1 0 0 8f' when NSEC3 non-narrow mode is used")
+		} else if err == nil {
+			if strings.ToLower(ns3p) != "1 0 0 8f" {
+				return fmt.Errorf("PowerDNS must have NSEC3PARAM option for domain set to '1 0 0 8f' when NSEC3 non-narrow mode is used")
+			}
+		}
+	default:
+		panic("unsupported NSEC mode")
+	}
+
 	// Determine current state of database
 	var curBlockHash = mainNetGenesisHash
 	var curBlockHeight = genesisHeight
@@ -85,66 +195,16 @@ func Run(cfg Config, startedNotifyFunc func() error) error {
 		return err
 	}
 
-	// Prepare statements
-	prGetDomainID, err := db.Prepare("SELECT id FROM namecoin_domains WHERE name=$1 LIMIT 1")
+	pr := prepareds{}
+	err = pr.Prepare(db)
 	if err != nil {
 		return err
 	}
-	prInsertDomain, err := db.Prepare("INSERT INTO namecoin_domains (name,last_height) VALUES ($1,$2) RETURNING id")
-	if err != nil {
-		return err
-	}
-	prUpdateDomainHeight, err := db.Prepare("UPDATE namecoin_domains SET last_height=$1 WHERE id=$2")
-	if err != nil {
-		return err
-	}
-	prDeleteDomainRecords, err := db.Prepare("DELETE FROM records WHERE namecoin_domain_id=$1")
-	if err != nil {
-		return err
-	}
-	prUpdateState, err := db.Prepare("UPDATE namecoin_state SET cur_block_hash=$1, cur_block_height=$2")
-	if err != nil {
-		return err
-	}
-	prInsertPrevBlock, err := db.Prepare("INSERT INTO namecoin_prevblock (block_hash, block_height) VALUES ($1,$2) RETURNING id")
-	if err != nil {
-		return err
-	}
-	prTruncatePrevBlock, err := db.Prepare("DELETE FROM namecoin_prevblock WHERE id < $1")
-	if err != nil {
-		return err
-	}
-	prInsertRecord, err := db.Prepare("INSERT INTO records (domain_id,ttl,prio,namecoin_domain_id,name,type,content) VALUES (1,600,$1,$2,$3,$4,$5)")
-	if err != nil {
-		return err
-	}
-	prRollback, err := db.Prepare("SELECT block_hash, block_height FROM namecoin_prevblock ORDER BY id DESC LIMIT 1")
-	if err != nil {
-		return err
-	}
-	prRollbackPop, err := db.Prepare("DELETE FROM namecoin_prevblock WHERE id IN (SELECT id FROM namecoin_prevblock ORDER BY id DESC LIMIT 1)")
-	if err != nil {
-		return err
-	}
-	prRollbackNewer, err := db.Prepare("SELECT id, name FROM namecoin_domains WHERE last_height > $1")
-	if err != nil {
-		return err
-	}
-	prDeleteName, err := db.Prepare("DELETE FROM namecoin_domains WHERE id=$1")
-	if err != nil {
-		return err
-	}
-
-	var prNotify *sql.Stmt
-	if cfg.Notify {
-		prNotify, err = db.Prepare("SELECT pg_notify('namecoin_updated', $1)")
-		if err != nil {
-			return err
-		}
-	}
+	defer pr.Close()
 
 	// Main loop
 	var tx *sql.Tx
+	var tpr *prepareds
 	lastStatusUpdateTime := time.Time{}
 	diverging := false
 
@@ -155,13 +215,36 @@ func Run(cfg Config, startedNotifyFunc func() error) error {
 			if err != nil {
 				return err
 			}
+			tpr, err = pr.Tx(tx)
+			if err != nil {
+				return err
+			}
 		}
 		return nil
 	}
 
+	defer func() {
+		if tpr != nil {
+			tpr.Close()
+		}
+	}()
+
+	iterStartTime := time.Now()
+	prevBlockHeight := curBlockHeight
+
 	for {
-		log.Info(fmt.Sprintf("SYNC curHeight=%d", curBlockHeight))
-		events, err := c.Sync(curBlockHash, 1000, true)
+		newNow := time.Now()
+		timeTaken := newNow.Sub(iterStartTime)
+		blockInc := curBlockHeight - prevBlockHeight
+		if blockInc == 0 {
+			blockInc = 1
+		}
+		timePerBlock := timeTaken.Seconds() / float64(blockInc)
+		log.Info(fmt.Sprintf("SYNC curHeight=%6d  %2.2fs  %6d  %7.0f blocks/s", curBlockHeight, timeTaken.Seconds(), blockInc, 1/timePerBlock))
+		iterStartTime = newNow
+		prevBlockHeight = curBlockHeight
+
+		events, err := c.Sync(curBlockHash, 10000, true)
 		if err == namecoin.ErrSyncNoSuchBlock {
 			// Rollback mode.
 			diverging = true
@@ -171,7 +254,7 @@ func Run(cfg Config, startedNotifyFunc func() error) error {
 				return err
 			}
 
-			err = tx.Stmt(prRollback).QueryRow().Scan(&curBlockHash, &curBlockHeight)
+			err = tpr.Rollback.QueryRow().Scan(&curBlockHash, &curBlockHeight)
 			if err != nil {
 				// - we have run out of prevblocks
 				// - any other error
@@ -179,7 +262,7 @@ func Run(cfg Config, startedNotifyFunc func() error) error {
 				return err
 			}
 
-			_, err = tx.Stmt(prRollbackPop).Exec()
+			_, err = tpr.RollbackPop.Exec()
 			if err != nil {
 				return err
 			}
@@ -192,7 +275,7 @@ func Run(cfg Config, startedNotifyFunc func() error) error {
 
 		if diverging {
 			// We have finished the rollback process and found the divergence point.
-			rows, err := tx.Stmt(prRollbackNewer).Query(curBlockHeight)
+			rows, err := tpr.RollbackNewer.Query(curBlockHeight)
 			if err != nil {
 				return err
 			}
@@ -216,7 +299,7 @@ func Run(cfg Config, startedNotifyFunc func() error) error {
 				v, err := c.Query(nName)
 				if err != nil {
 					// assume the name does not exist anymore, delete it
-					_, err = tx.Stmt(prDeleteName).Exec(domainID)
+					_, err = tpr.DeleteName.Exec(domainID)
 					if err != nil {
 						return err
 					}
@@ -235,13 +318,13 @@ func Run(cfg Config, startedNotifyFunc func() error) error {
 				// (The name from name_show might actually be newer than this height, but if so it will
 				//  rapidly be fixed. This avoids the need to change the .Query() interface to provide
 				//  height.)
-				_, err = tx.Stmt(prUpdateDomainHeight).Exec(curBlockHeight, domainID)
+				_, err = tpr.UpdateDomain.Exec(curBlockHeight, v, domainID)
 				if err != nil {
 					return err
 				}
 
 				// Delete the old records for this domain.
-				_, err = tx.Stmt(prDeleteDomainRecords).Exec(domainID)
+				_, err = tpr.DeleteDomainRecords.Exec(domainID)
 				if err != nil {
 					return err
 				}
@@ -249,23 +332,29 @@ func Run(cfg Config, startedNotifyFunc func() error) error {
 				// Determine the new records for this domain.
 				// We don't count each domain as its own zone, so we don't add SOA
 				// records and use NS records only where a delegation is requested.
-				rrs, err := backend.Convert(dnsNameFull, v)
+				vv, err := ncdomain.ParseValue(v, nil)
+				if err != nil {
+					continue
+				}
+
+				rrs, err := vv.RRsRecursive(nil, dnsNameFull)
 				if err != nil {
 					//log.Info(fmt.Sprintf("Couldn't process domain \"%s\": %s", ev.Name, err))
 					continue
 				}
 
 				// Add the new records for this domain.
-				for _, rr := range rrs {
-					err = insertRR(tx.Stmt(prInsertRecord), rr, domainID)
-					if err != nil {
-						return err
-					}
+				err = insertRRs(tpr.InsertRecord, rrs, domainID, cfg.NSECMode)
+				if err != nil {
+					return err
 				}
 			}
 
 			rows.Close()
 			rows = nil
+
+			tpr.Close()
+			tpr = nil
 
 			err = tx.Commit()
 			if err != nil {
@@ -293,7 +382,7 @@ func Run(cfg Config, startedNotifyFunc func() error) error {
 
 		catchUpThreshold := maxBlockHeightAtStart - 2
 		catchingUp := curBlockHeight < catchUpThreshold
-		doNotify := (prNotify != nil && !catchingUp)
+		doNotify := (cfg.Notify && !catchingUp)
 
 		if cfg.StatusUpdateFunc != nil {
 			now := time.Now()
@@ -314,13 +403,6 @@ func Run(cfg Config, startedNotifyFunc func() error) error {
 				return err
 			}
 
-			/*if tx == nil {
-				tx, err = db.Begin()
-				if err != nil {
-					return err
-				}
-			}*/
-
 			switch ev.Type {
 			case "update", "firstupdate":
 				var domainID int64
@@ -335,21 +417,21 @@ func Run(cfg Config, startedNotifyFunc func() error) error {
 
 				// Determine the namecoin domain ID, creating the record if it doesn't already exist.
 				// Update the block height to the current value.
-				err = tx.Stmt(prGetDomainID).QueryRow(ev.Name).Scan(&domainID)
+				err = tpr.GetDomainID.QueryRow(ev.Name).Scan(&domainID)
 				if err != nil {
-					err = tx.Stmt(prInsertDomain).QueryRow(ev.Name, curBlockHeight).Scan(&domainID)
+					err = tpr.InsertDomain.QueryRow(ev.Name, curBlockHeight, ev.Value).Scan(&domainID)
 					if err != nil {
 						return err
 					}
 				} else {
-					_, err = tx.Stmt(prUpdateDomainHeight).Exec(curBlockHeight, domainID)
+					_, err = tpr.UpdateDomain.Exec(curBlockHeight, ev.Value, domainID)
 					if err != nil {
 						return err
 					}
 				}
 
 				// Delete the old records for this domain.
-				_, err = tx.Stmt(prDeleteDomainRecords).Exec(domainID)
+				_, err = tpr.DeleteDomainRecords.Exec(domainID)
 				if err != nil {
 					return err
 				}
@@ -361,34 +443,37 @@ func Run(cfg Config, startedNotifyFunc func() error) error {
 				// Determine the new records for this domain.
 				// We don't count each domain as its own zone, so we don't add SOA
 				// records and use NS records only where a delegation is requested.
-				rrs, err := backend.Convert(dnsNameFull, ev.Value)
+				vv, err := ncdomain.ParseValue(ev.Value, nil)
+				if err != nil {
+					continue
+				}
+
+				rrs, err := vv.RRsRecursive(nil, dnsNameFull)
 				if err != nil {
 					//log.Info(fmt.Sprintf("Couldn't process domain \"%s\": %s", ev.Name, err))
 					continue
 				}
 
 				// Add the new records for this domain.
-				for _, rr := range rrs {
-					err = insertRR(tx.Stmt(prInsertRecord), rr, domainID)
-					if err != nil {
-						return err
-					}
+				err = insertRRs(tpr.InsertRecord, rrs, domainID, cfg.NSECMode)
+				if err != nil {
+					return err
 				}
 
 			case "atblock":
-				_, err = tx.Stmt(prUpdateState).Exec(ev.BlockHash, ev.BlockHeight)
+				_, err = tpr.UpdateState.Exec(ev.BlockHash, ev.BlockHeight)
 				if err != nil {
 					return err
 				}
 
 				var prevBlockID int64
-				err = tx.Stmt(prInsertPrevBlock).QueryRow(ev.BlockHash, ev.BlockHeight).Scan(&prevBlockID)
+				err = tpr.InsertPrevBlock.QueryRow(ev.BlockHash, ev.BlockHeight).Scan(&prevBlockID)
 				if err != nil {
 					return err
 				}
 
 				if prevBlockID > numPrevBlocksToKeep {
-					_, err = tx.Stmt(prTruncatePrevBlock).Exec(prevBlockID - numPrevBlocksToKeep)
+					_, err = tpr.TruncatePrevBlock.Exec(prevBlockID - numPrevBlocksToKeep)
 					if err != nil {
 						return err
 					}
@@ -400,14 +485,14 @@ func Run(cfg Config, startedNotifyFunc func() error) error {
 					for k := range updatedSet {
 						updateds += " " + k
 						if len(updateds) > 6000 {
-							_, err = tx.Stmt(prNotify).Exec(updateds)
+							_, err = tpr.Notify.Exec(updateds)
 							log.Infoe(err)
 							updateds = "inv"
 						}
 					}
 
 					if len(updateds) > 3 {
-						_, err = tx.Stmt(prNotify).Exec(updateds)
+						_, err = tpr.Notify.Exec(updateds)
 						log.Infoe(err)
 					}
 					// don't care about errors
@@ -418,6 +503,9 @@ func Run(cfg Config, startedNotifyFunc func() error) error {
 
 				// Commit.
 				if !skipCommit {
+					tpr.Close()
+					tpr = nil
+
 					err = tx.Commit()
 					if err != nil {
 						return err
@@ -433,11 +521,15 @@ func Run(cfg Config, startedNotifyFunc func() error) error {
 	}
 }
 
-func insertRR(stmt *sql.Stmt, rr dns.RR, domainID int64) error {
+func insertRR(stmt *sql.Stmt, rr dns.RR, domainID int64, hasNS bool, depthBelowNS int, nsecMode string) error {
 	prio := 0
 	hdr := rr.Header()
 	stype := dns.TypeToString[hdr.Rrtype]
 	value := ""
+
+	if stype == "" {
+		panic(fmt.Sprintf("no rr type: %+v", rr))
+	}
 
 	switch hdr.Rrtype {
 	case dns.TypeA:
@@ -453,18 +545,127 @@ func insertRR(stmt *sql.Stmt, rr dns.RR, domainID int64) error {
 		ds := rr.(*dns.DS)
 		value = fmt.Sprintf("%d %d %d %s", ds.KeyTag, ds.Algorithm, ds.DigestType, strings.ToUpper(ds.Digest))
 
-	//case dns.TypeSRV:
-	//case dns.TypeTXT:
+	case dns.TypeCNAME:
+		cname := rr.(*dns.CNAME)
+		value = normalizeName(cname.Target)
+
+	case dns.TypeDNAME:
+		dname := rr.(*dns.DNAME)
+		value = normalizeName(dname.Target)
+
+	case dns.TypeSRV:
+		srv := rr.(*dns.SRV)
+		prio = int(srv.Priority)
+		value = fmt.Sprintf("%d %d %s", srv.Weight, srv.Port, normalizeName(srv.Target))
+
+	case dns.TypeMX:
+		mx := rr.(*dns.MX)
+		prio = int(mx.Preference)
+		value = normalizeName(mx.Mx)
+
+	case dns.TypeTXT:
+		txt := rr.(*dns.TXT)
+
+		first := true
+		for _, t := range txt.Txt {
+			if !first {
+				value += " "
+			}
+			first = false
+			value += "\""
+			for _, c := range t {
+				if c == '"' {
+					value += "\""
+				} else {
+					value += string(c)
+				}
+			}
+			value += "\""
+		}
+
 	default:
-		return fmt.Errorf("unsupported record type")
+		return fmt.Errorf("unsupported record type: %s", rr.Header())
 	}
 
-	_, err := stmt.Exec(prio, domainID, normalizeName(hdr.Name), stype, value)
+	auth := !hasNS || (rr.Header().Rrtype == dns.TypeDS && depthBelowNS == 0)
+
+	ordername := sql.NullString{}
+	switch nsecMode {
+	case "nsec3narrow":
+	case "nsec3":
+		ordername.String = strings.ToLower(dns.HashName(rr.Header().Name, dns.SHA1, 0, "8F"))
+		ordername.Valid = true
+	case "nsec":
+		panic("unsupported NSEC mode")
+	default:
+		panic("unsupported NSEC mode")
+	}
+
+	_, err := stmt.Exec(prio, domainID, normalizeName(hdr.Name), stype, value, auth, ordername)
 	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func insertRRs(stmt *sql.Stmt, rrs []dns.RR, domainID int64, nsecMode string) error {
+	nsSuffix := []string{}
+
+	for _, rr := range rrs {
+		// XXX: this currently relies on a specific ordering of RRs:
+		//      a.b.c
+		//      x.a.b.c
+		//      1.x.a.b.c
+		//      2.x.a.b.c
+		//      y.a.b.c
+		//      b.b.c
+		//      plus the assumption that at a given owner name, NS records
+		//      always come first.
+		//      RRsRecursive currently gives this ordering.
+
+		// We are no longer in the current delegation level, pop it off.
+		name := normalizeName(rr.Header().Name)
+
+		if len(nsSuffix) > 0 && !domainHasSuffix(name, nsSuffix[len(nsSuffix)-1]) {
+			nsSuffix = nsSuffix[0 : len(nsSuffix)-1]
+		}
+
+		// Enter a new delegation level.
+		if rr.Header().Rrtype == dns.TypeNS {
+			nsSuffix = append(nsSuffix, name)
+		}
+
+		depthBelowNS := 0
+		if len(nsSuffix) > 0 {
+			subPart := normalizeName(name[0 : len(name)-len(nsSuffix[len(nsSuffix)-1])])
+			depthBelowNS = strings.Count(subPart, ".")
+			if len(subPart) > 0 {
+				depthBelowNS++
+			}
+		}
+
+		err := insertRR(stmt, rr, domainID, len(nsSuffix) > 0, depthBelowNS, nsecMode)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func domainHasSuffix(x, y string) bool {
+	x = normalizeName(x)
+	y = normalizeName(y)
+	if x == y {
+		return true
+	}
+	return strings.HasSuffix(x, "."+y)
+}
+
+func nameIsTopLevel(n string) bool {
+	n = normalizeName(n)
+	return strings.Count(n, ".") == 1
 }
 
 func normalizeName(n string) string {
